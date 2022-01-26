ea0d545e1744012a3fae00bac20b6d7d4aa1250f
JoelGRod <joelgrod@gmail.com>
2022-01-26 22:37:44 +0000

    dna_chain

--- src/algorithms/main/strings/dna_chain.py
+++ src/algorithms/main/strings/dna_chain.py
@@ -1,3 +1,18 @@
+"""
+    Deoxyribonucleic acid (DNA) is a chemical found in the nucleus of cells 
+    and carries the "instructions" for the development and functioning of 
+    living organisms.
+    
+    If you want to know more: http://en.wikipedia.org/wiki/DNA
+    
+    In DNA strings, symbols "A" and "T" are complements of each other, as 
+    "C" and "G". You function receives one side of the DNA (string, except 
+    for Haskell); you need to return the other complementary side. DNA 
+    strand is never empty or there is no DNA at all 
+    (again, except for Haskell).
+"""
+
+
 def DNA_strand(dna):
     dna_comp = {
         "A": "T",
@@ -5,5 +20,5 @@ def DNA_strand(dna):
         "C": "G",
         "G": "C"
     }
-    
+
     return "".join(list(map(lambda letter: dna_comp[letter], dna)))
