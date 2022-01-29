eca0b2881594d4f9d28460b8a53c10e7fb8d76cd
JoelGRod <joelgrod@gmail.com>
2022-01-29 01:16:44 +0000

    sort the odds

--- src/algorithms/main/maths/sort_the_odd.py
+++ src/algorithms/main/maths/sort_the_odd.py
@@ -9,13 +9,26 @@ Examples
 [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  =>  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
 """
 
+
 def sort_array(source_array):
     # Return a sorted array.
-    numbers = list(map(lambda number: 
-                       " " if number % 2 != 0 
+    numbers = list(map(lambda number:
+                       " " if number % 2 != 0
                        else number, source_array))
     odds = sorted([odd for odd in source_array if odd % 2 != 0])
-    return list(map(lambda number: 
-                       odds.pop(0) 
-                       if number == " " 
-                       else number, numbers))
+    return list(map(lambda number:
+                    odds.pop(0)
+                    if number == " "
+                    else number, numbers))
+
+
+def sort_array_extra(arr):
+    odds = sorted((x for x in arr if x % 2 != 0), reverse=True)
+    return [x if x % 2 == 0 else odds.pop() for x in arr]
+
+
+def sort_array_extra_two(source_array):
+    odds = sorted([odd for odd in source_array if odd % 2 != 0])
+    return list(map(lambda number:
+                    number if number % 2 == 0
+                    else odds.pop(0), source_array))
