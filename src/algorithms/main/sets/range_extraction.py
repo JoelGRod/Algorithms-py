5a8c7afc73ed07de427aace731de26387b9a33b2
joelgrod <joelgrod@gmail.com>
2022-01-30 17:07:28 +0000

    range extraction

--- src/algorithms/main/sets/range_extraction.py
+++ src/algorithms/main/sets/range_extraction.py
@@ -37,3 +37,20 @@ def solution(args):
             ranges.append(f"{nums[0]}")
 
     return ",".join(ranges)
+
+def solution_extra(args):
+    out = []
+    beg = end = args[0]
+    
+    for n in args[1:] + [""]:        
+        if n != end + 1:
+            if end == beg:
+                out.append( str(beg) )
+            elif end == beg + 1:
+                out.extend( [str(beg), str(end)] )
+            else:
+                out.append( str(beg) + "-" + str(end) )
+            beg = n
+        end = n
+    
+    return ",".join(out)
